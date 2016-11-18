c4e8de28ea0eb905154738671aa7dff24f60e6ef
que01 <que01@foxmail.com>
2016-11-19 03:04:33 +0800

    init commit

---
+++ src/routes/IndexPage.js
@@ -0,0 +1,22 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'dva';
+import { Link } from 'dva/router';
+import styles from './IndexPage.css';
+
+function IndexPage() {
+  return (
+    <div className={styles.normal}>
+      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
+      <div className={styles.welcome} />
+      <ul className={styles.list}>
+        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
+        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md" target="_blank">Getting Started</a></li>
+      </ul>
+    </div>
+  );
+}
+
+IndexPage.propTypes = {
+};
+
+export default connect()(IndexPage);
