88960eadc540fd25ebfbac5368f3be57a40ca52d
que01 <que01@foxmail.com>
2016-11-19 03:21:10 +0800

    基础栅格

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -3,53 +3,20 @@ import { connect } from 'dva';
 import { Link } from 'dva/router';
 import styles from './IndexPage.css';
 
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
-const FormItem = Form.Item;
+import { Row, Col } from 'antd';
 
-const NormalLoginForm = Form.create()(React.createClass({
-  handleSubmit(e) {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
-  },
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    return (
-	  <div style={{width:'400px',margin:"0 auto"}}>
-		  <Form onSubmit={this.handleSubmit} className="login-form">
-			<FormItem>
-			  {getFieldDecorator('userName', {
-				rules: [{ required: true, message: 'Please input your username!' }],
-			  })(
-				<Input addonBefore={<Icon type="user" />} placeholder="Username" />
-			  )}
-			</FormItem>
-			<FormItem>
-			  {getFieldDecorator('password', {
-				rules: [{ required: true, message: 'Please input your Password!' }],
-			  })(
-				<Input addonBefore={<Icon type="lock" />} type="password" placeholder="Password" />
-			  )}
-			</FormItem>
-			<FormItem>
-			  {getFieldDecorator('remember', {
-				valuePropName: 'checked',
-				initialValue: true,
-			  })(
-				<Checkbox>Remember me</Checkbox>
-			  )}
-			  <a className="login-form-forgot">Forgot password</a>
-			  <Button type="primary" htmlType="submit" className="login-form-button">
-				Log in
-			  </Button>
-			  Or <a>register now!</a>
-			</FormItem>
-		  </Form>
-	</div>
-    );
-  },
-}));
-export default connect()(NormalLoginForm) ;
+class App extends Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <Row style={{width:'1000px',margin:'0 auto'}}>
+        <Col span={6}>col-12</Col>
+        <Col span={18}>col-12</Col>
+      </Row>
+    )
+  }
+}
+
+export default connect()(App) ;
