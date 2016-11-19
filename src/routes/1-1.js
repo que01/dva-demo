136322e139d139f035fcc0e66667fcd86729b628
que01 <que01@foxmail.com>
2016-11-19 12:23:15 +0800

    加一个写死的列表组件

--- src/routes/1-1.js
+++ src/routes/1-1.js
@@ -1,11 +1,63 @@
 import React from 'react';
 import { connect } from 'dva';
+import {Table,Icon} from 'antd';
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: text => <a href="#">{text}</a>,
+  }, {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  }, {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  }, {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <span>
+        <a href="#">Action 一 {record.name}</a>
+        <span className="ant-divider" />
+        <a href="#">Delete</a>
+        <span className="ant-divider" />
+        <a href="#" className="ant-dropdown-link">
+          More actions<Icon type="down" />
+        </a>
+      </span>
+    ),
+  }
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+  }, {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  }, {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+  }
+];
+
 class Option extends React.Component{
   constructor(props){
     super(props)
   }
   render(){
-    return (<div>菜单一1</div>)
+    return (<Table columns={columns} dataSource={data} />)
   }
   componentDidMount(){
     const breadcrumbData = {
@@ -24,7 +76,6 @@ class Option extends React.Component{
     })
   }
 }
-
 function mapStateToProps({ common }) {
   return {common};
 }
